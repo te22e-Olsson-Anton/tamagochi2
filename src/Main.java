c5caa53ba7b23b03295c39c9fcb5cec00e3eebcd
anton.olsson1 <anton.olsson3@elev.ga.ntig.se>
2024-10-10 11:02:56 +0200

    nästan klar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner tangentbord = new Scanner(System.in);
+
+        Tamagochi tamagochi = new Tamagochi();
+        tamagochi.name = "Bob";
+
+        boolean startGame = false;
+
+        System.out.println("Tryck 1 för att starta spelet");
+
+        String input1 = tangentbord.nextLine();
+
+        if (input1.equals("1")) {
+            startGame = true;
+            System.out.println("Spelet har börjat! Du kontrollerar " + tamagochi.name);
+        }
+
+        while (startGame) {
+            System.out.println("\nVad vill du göra?");
+            System.out.println("1. Lära ett nytt ord");
+            System.out.println("2. Mata Tamagotchi");
+            System.out.println("3. Säg hej");
+            System.out.println("4. Visa status");
+            System.out.println("5. Gör inget");
+            System.out.println("6. Avsluta");
+
+            String choice = tangentbord.nextLine();
+
+            switch (choice)
+            {
+                case "1":
+                    System.out.println("Vilket ord vill du lära " + tamagochi.name + "?");
+                    String wordToTeach = tangentbord.nextLine();
+                    tamagochi.teachWord(wordToTeach);
+                    tamagochi.tick();
+                    break;
+
+                case "2":
+                    tamagochi.feed();
+                    tamagochi.tick();
+                    break;
+
+                case "3":
+                    tamagochi.hi();
+                    tamagochi.tick();
+                    break;
+
+                case "4":
+                    tamagochi.printStats();
+                    tamagochi.tick();
+                    break;
+
+                case "5":
+                    System.out.println("Gör inget");
+                    tamagochi.tick();
+                    break;
+
+                case "6":
+                    System.out.println("Spelet avslutas.");
+                    startGame = false;
+                    break;
+
+                default:
+                    System.out.println("Ogiltigt val, försök igen.");
+                    break;
+            }
+
+            if (tamagochi.hunger == 10 || tamagochi.boredom == 10)
+            {
+                tamagochi.isAlive = false;
+            }
+
+            if(!tamagochi.isAlive)
+            {
+                startGame = false;
+                System.out.println(tamagochi.name + " har dött");
+            }
+        }
+
+        tangentbord.close();
     }
 }
--- src/Tamagochi.java
+++ src/Tamagochi.java
@@ -1,4 +1,74 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class Tamagochi {
+public class Tamagochi
+{
+    int hunger = 0;
+    int boredom = 0;
+
+    List<String> words = new ArrayList<>();
+
+    boolean isAlive = true;
+    String name;
+
+
+
+
+
+    public void feed()
+    {
+
+
+        if (hunger > 0)
+        {
+            hunger =+ 2;
+            System.out.println(name + "är nu mindre hungrig. Hunger: " + hunger);
+        }
+        else
+        {
+            System.out.println(name + " är inte hungrig");
+        }
+    }
+    public void hi()
+    {
+        if (words.isEmpty())
+        {
+            System.out.println(name + " har inte lärt sig några ord ännu.");
+        } else
+            {
+            Random random = new Random();
+            String randomWord = words.get(random.nextInt(words.size()));
+            System.out.println(name + " säger: " + randomWord);
+            reduceBoredom();
+            }
+        }
+    public void teachWord(String Word)
+    {
+        words.add(Word);
+
+        System.out.println(name + " har lärt sig ordet " + Word);
+    }
+    public void tick()
+    {
+        hunger++;
+        boredom++;
+    }
+    public void printStats()
+    {
+        System.out.println("hunger är " + hunger);
+        System.out.println("Boredom är " + boredom);
+    }
+    public void reduceBoredom()
+    {
+        if (boredom > 0)
+        {
+            boredom =+ 2;
+            System.out.println(name + "har nu mindre tråkigt. Boredom: " + boredom);
+        }
+        else
+        {
+            System.out.println(name + " har inte tråkigt");
+        }
+    }
 }
